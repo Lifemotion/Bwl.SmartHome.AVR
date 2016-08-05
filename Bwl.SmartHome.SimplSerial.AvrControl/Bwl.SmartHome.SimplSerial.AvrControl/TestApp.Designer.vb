a25516813c790487498b99d3b402537b67358484
Igor Koshelev <igor@cleverflow.ru>
2016-08-05 19:35:04 +0300

    (no message)

---
+++ Bwl.SmartHome.SimplSerial.AvrControl/Bwl.SmartHome.SimplSerial.AvrControl/TestApp.Designer.vb
@@ -0,0 +1,43 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TestApp
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'logWriter
+        '
+        Me.logWriter.Location = New System.Drawing.Point(2, 291)
+        Me.logWriter.Size = New System.Drawing.Size(781, 269)
+        '
+        'TestApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Name = "TestApp"
+        Me.Text = "Bwl SmartHome SimplSerial AVR"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+End Class
---
+++ Bwl.SmartHome.SimplSerial.AvrControl/Bwl.SmartHome.SimplSerial.AvrControl/TestApp.vb
@@ -0,0 +1,7 @@
+﻿Imports Bwl.Framework
+
+Public Class TestApp
+    Inherits FormAppBase
+    Private _client As New SmartHomeClient(_storage, _logger)
+
+End Class
